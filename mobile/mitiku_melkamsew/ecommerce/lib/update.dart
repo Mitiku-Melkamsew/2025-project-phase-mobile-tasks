00600b20a32fc97164ac63c24d5903c497526d47
Mitiku Melkamsew <mitikumelkamsew@gmail.com>
2025-07-27 23:17:36 +0300

    enhanced the layout of the Update page for better user interaction

--- mobile/mitiku_melkamsew/ecommerce/lib/update.dart
+++ mobile/mitiku_melkamsew/ecommerce/lib/update.dart
@@ -5,127 +5,144 @@ class Update extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              width: double.infinity,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: Colors.grey, width: 1),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.image, size: 60, color: Colors.grey[700]),
-                  SizedBox(height: 10),
-                  Text(
-                    'Tap to upload image',
-                    style: TextStyle(color: Colors.grey[700], fontSize: 16),
+    var font = TextStyle(
+      color: Color(0xff3e3e3e),
+      fontSize: 14,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(
+          width: 40,
+          height: 40,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xffffffff),
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: Icon(
+            Icons.arrow_back_ios_rounded,
+            color: Color(0xff3F51F3),
+            size: 20,
+          ),
+        ),
+        title: Center(
+          child: Text('Add product', style: font.copyWith(fontSize: 16)),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: double.infinity,
+                  height: 190,
+                  decoration: BoxDecoration(
+                    color: Color(0xfff3f3f3),
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(color: Color(0xfff3f3f3), width: 1),
                   ),
-                ],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.image, size: 36, color: Colors.grey[700]),
+                      SizedBox(height: 10),
+                      Text('upload image', style: font),
+                    ],
+                  ),
+                ),
               ),
-            ),
+              SizedBox(height: 16),
+              Text('Name', style: font),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xfff3f3f3),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 5),
+              Text('Category', style: font),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xfff3f3f3),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              Text('Price', style: font),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xfff3f3f3),
+                  suffixText: '\$',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              Text('Description', style: font),
+              TextField(
+                maxLines: 3,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xfff3f3f3),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff3F51F3),
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  "ADD",
+                  style: font.copyWith(
+                    fontWeight: FontWeight.w600,
+                    color: Color(0x80ffffff),
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Color(0xffff1313)),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  'Delete', style: font.copyWith(fontWeight: FontWeight.w600, color: Color(0x80ff1313))
+                  
+                ),
+              ),
+            ],
           ),
-          SizedBox(height: 16),
-          Text('Name'),
-          TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.grey,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
-              ),
-            ),
-          ),
-          SizedBox(height: 5),
-          Text('Category'),
-          TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.grey,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
-              ),
-            ),
-          ),
-          SizedBox(height: 8),
-          Text('Price'),
-          TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.grey,
-              suffixText: '\$',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
-              ),
-            ),
-          ),
-          SizedBox(height: 8),
-          Text('Description'),
-          TextField(
-            maxLines: 4,
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.grey,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
-              ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // Your logic here
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue.shade700, // Blue background
-              minimumSize: Size(
-                double.infinity,
-                50,
-              ), // Full width, fixed height
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8), // Rounded corners
-              ),
-            ),
-            child: Text(
-              "ADD",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1,
-              ),
-            ),
-          ),
-
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              minimumSize: Size(double.infinity, 50),
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.red),
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            child: Text(
-              'Delete',
-              style: TextStyle(
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1,
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
