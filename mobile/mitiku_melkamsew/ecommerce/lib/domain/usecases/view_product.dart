62e9830c53d405214a1b0f1874b30b3f1681c908
Mitiku Melkamsew <mitikumelkamsew@gmail.com>
2025-07-31 11:01:57 +0300

    I have implemented domain layer refactoring. implemented product entity, repository and usecase.

---
+++ mobile/mitiku_melkamsew/ecommerce/lib/domain/usecases/view_product.dart
@@ -0,0 +1,13 @@
+import '../entities/product.dart';
+import '../repositories/product_repository.dart';
+
+class ViewProduct {
+  final ProductRepository repository;
+
+  ViewProduct(this.repository);
+
+  Future<Product> call(int id) async{
+    return await repository.getProductById(id);
+  }
+
+}
