8bc0b6903c22ba67e6315e70a6e18d3ba034e669
Mitiku Melkamsew <mitikumelkamsew@gmail.com>
2025-08-02 17:42:22 +0300

    Implemented product rebository and added local and remote datasources

--- mobile/mitiku_melkamsew/ecommerce/test/features/product/domain/usecases/get_product_test.dart
+++ mobile/mitiku_melkamsew/ecommerce/test/features/product/domain/usecases/get_product_test.dart
@@ -1,14 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:ecommerce/features/product/domain/entities/product.dart';
-import 'package:ecommerce/features/product/domain/repositories/product_repository.dart';
 import 'package:ecommerce/features/product/domain/usecases/get_product.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'get_product_test.mocks.dart';
+import '../../../../helper/mocks.mocks.dart';
 
-@GenerateMocks([ProductRepository])
 void main() {
   late GetProduct usecase;
   late MockProductRepository mockProductRepository;
