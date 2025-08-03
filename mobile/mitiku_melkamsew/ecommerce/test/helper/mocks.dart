d6861bb47c4322f9b608a3f546ceef4d77464cc3
Mitiku Melkamsew <mitikumelkamsew@gmail.com>
2025-08-03 06:27:57 +0300

    Implemented network info implementation

--- mobile/mitiku_melkamsew/ecommerce/test/helper/mocks.dart
+++ mobile/mitiku_melkamsew/ecommerce/test/helper/mocks.dart
@@ -1,8 +1,15 @@
-import 'package:ecommerce/core/platform/network_info.dart';
+import 'package:ecommerce/core/network/network_info.dart';
 import 'package:ecommerce/features/product/data/data_sources/product_local_data_source.dart';
 import 'package:ecommerce/features/product/data/data_sources/product_remote_data_source.dart';
 import 'package:ecommerce/features/product/domain/repositories/product_repository.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:mockito/annotations.dart';
 
-@GenerateMocks([ProductRepository, ProductLocalDataSource, ProductRemoteDataSource, NetworkInfo])
-void main(){}
+@GenerateMocks([
+  ProductRepository,
+  ProductLocalDataSource,
+  ProductRemoteDataSource,
+  NetworkInfo,
+  InternetConnectionChecker,
+])
+void main() {}
